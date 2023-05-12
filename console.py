635652eb8117f9a703d1d568296156db101746cc
Mohammednur <mamebb2023@gmail.com>
2023-05-12 18:18:57 +0300

    The console

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" The command prompt for the site """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ The command prompt """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ Quit command to exit the program """
+        return True
+    
+    def do_EOF(self, arg):
+        """ Exit command to exit the program """
+        return True
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
