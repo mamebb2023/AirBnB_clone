1dcb6f66dcb3fe3b6e531c7196f581fde7b2c922
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-05-12 14:18:13 +0100

    Removed model storage test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -19,9 +19,10 @@ class TestBaseModel_instantiation(unittest.TestCase):
 
     def test_no_args_instantiates(self):
         self.assertEqual(BaseModel, type(BaseModel()))
-
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(BaseModel(), models.storage.all().values())
+    
+    # No storage associate yet.
+    # def test_new_instance_stored_in_objects(self):
+    #     self.assertIn(BaseModel(), models.storage.all().values())
 
     def test_id_is_public_str(self):
         self.assertEqual(str, type(BaseModel().id))
