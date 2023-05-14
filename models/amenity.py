40dd29b713d16dea1774a89f1486d0dc7f4c8226
Mohammednur <mamebb2023@gmail.com>
2023-05-14 15:02:08 +0300

    Classes

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Amenity class """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Defines the class Amenity """
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" State class """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Defines the class State """
+
+    name = ""
